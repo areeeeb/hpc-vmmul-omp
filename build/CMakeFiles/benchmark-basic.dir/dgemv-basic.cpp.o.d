f0cf93f2570980115b2a2bbe8b3ab3f243b67475
areeb ahmed abbasi <areeb@x1004c4s5b0n1h0.chn.perlmutter.nersc.gov>
2024-10-07 20:42:00 -0700

    build version on perlmutter

---
+++ build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
+ /global/homes/a/areeb/assignment_3/hpc-vmmul-omp/dgemv-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
+ /global/homes/a/areeb/assignment_3/hpc-vmmul-omp/dgemv-vectorized.cpp \
+ /usr/include/stdc-predef.h
